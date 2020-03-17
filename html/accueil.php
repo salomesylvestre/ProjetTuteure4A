558143b48273e14380cecda79a9bd83c4a73977b
salomesylvestre <salome.sylvestregrosmaurice@etud.univ-jfc.fr>
2020-03-17 05:06:13 -0600

    page_rdv

--- html/accueil.php
+++ html/accueil.php
@@ -3,7 +3,7 @@
 session_start();
 try {
     // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=cr;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=cr2;charset=utf8;port=3308', 'root', '');
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
     die('Erreur : ' . $e->getMessage());
@@ -18,9 +18,12 @@ try {
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
+    <script src="https://kit.fontawesome.com/81aa4e8517.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <title></title>
 
 
@@ -29,11 +32,14 @@ try {
         <a class="navbar-brand" href="#">
             <img src="../image/logo64.png" alt="" class="rounded-circle">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-bars fa-lg" style="color:#e3001b; font-size:28px;"></i>
+            </span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <a class="nav-link " href="#">Accueil </a>
                 </li>
@@ -61,12 +67,9 @@ try {
                     <li class="nav-item"><a class="nav-link" href="connexion.php">Se connecter</a></li>
                 <?php
                 } ?>
-
                 <?php if (isset($_SESSION['id_type']) and ($_SESSION['id_type']) == 1) { ?>
                     <li class="nav-item"><a class="nav-link" href="inscription.php">Inscrire une personne
                             <?php ?></a></li><?php } ?>
-
-
             </ul>
             </ul>
         </div>
@@ -77,10 +80,72 @@ try {
 
 
 <body>
-    <h1 class="title"><span>Vehicule-Red</span></h1>
 
-    <input type="submit" id='submit' value='Se connecter' >
-    <input type="button" id='submit' value="Obtenir des informations" >
-</body>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+          <h1 class="display-4 font-weight-normal">Vehicule-help</h1>
+          <p class="lead font-weight-normal">Prenez la route pour venir en aide à de nombreuses personnes.</p>
+          <a class="btn btn-outline-secondary" href="#">Se connecter</a>
+        </div>
+      </div>
+  
+      <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+        <div class="bg-danger mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+          <div class="my-3 py-3">
+            <h2 class="display-5">Être bénévole</h2>
+            <p class="lead">Donnez de votre aide selon votre propre emploi du temps. Conduisez quand et où vous le voulez. De plus, c'est vous qui choisissez les trajets.Avec l'app help, c'est vous qui décidez de votre horaire.</p>
+          </div>
+
+        </div>
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+          <div class="my-3 p-3">
+            <h2 class="display-5"> Être bénéficiaire</h2>
+            <p class="lead">Des trajets fiables en quelques minutes et à n'importe quel moment, a vos de décider</p>
+          </div>
+          
+        </div>
+        
+      </div>
+      <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-6 p-lg-7 mx-auto my-2">
+          <h1 class="display-5 font-weight-normal">Nous aidons plus de 35 villes dans toute la France</h1>
+          <p class="lead font-weight-normal">Prenez la route pour venir en aide à de nombreuses personnes.</p>
+          
+        </div>
+      </div>
+      <footer class="container py-5">
+        <div class="row">
+          <div class="col-12 col-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>App</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Centre d'aide</a></li>
+              <li><a class="text-muted" href="#">Français</a></li>
+              <li><a class="text-muted" href="#">Castres</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>A propos de nous</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Resources</a></li>
+              <li><a class="text-muted" href="#">Sécurité</a></li>
+              <li><a class="text-muted" href="#">Blog</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li>
+            </ul>
+          </div>
+
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Confidentialité</a></li>
+              <li><a class="text-muted" href="#">Accessibilité</a></li>
+              <li><a class="text-muted" href="#">Conditions</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
 
 </html>
--- html/rdv.php
+++ html/rdv.php
@@ -3,13 +3,46 @@
 session_start();
 try {
     // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=cr;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=cr2;charset=utf8;port=3308', 'root', '');
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
     die('Erreur : ' . $e->getMessage());
-} ?>
+}
+
+if (isset($_POST['formrdv'])) {
+    $motif = htmlspecialchars($_POST['motif']);
+    $lieu = htmlspecialchars($_POST['lieu']);
+    $date = ($_POST['date']);
+    $heure = ($_POST['heure']);
+    $heure_dep = ($_POST['heure_dep']);
+    $adresse_dep = htmlspecialchars($_POST['adresse_dep']);
+    $heure_ret = ($_POST['heure_ret']);
+
+    if (
+        !empty($_POST['motif']) and !empty($_POST['lieu']) and !empty($_POST['date']) and
+        !empty($_POST['heure']) and !empty($_POST['heure_dep']) and !empty($_POST['adresse_dep'])
+        and !empty($_POST['heure_ret'])
+    ) {
+if ($date>date("Y-d-m")){
+        if ($heure_dep < $heure) {
+            if ($heure < $heure_ret) {
+                $insertRDV = $bdd->prepare("INSERT INTO RDV(LIEU_DEPART,LIEU_RDV,MOTIF,DATE_RDV,HEURE_DEPART,HEURE_RETOUR,HEURE_RDV) values(?,?,?,?,?,?,?) ");
+                $insertRDV->execute(array($adresse_dep, $lieu, $motif, $date, $heure_dep, $heure_ret, $heure));
+                header('Location: suivi.php?=' . $userinfo['id_type']);
+            } else {
+                $erreur = "L'heure de retour doit être après l'heure du rendez-vous";
+            }
+        } else {
+
+            $erreur = "L'heure de départ doit être avant l'heure du rendez-vous";
+        }
+    }
+    else $erreur="La date que vous avez choisi est déjà passée !";
+}}
 
 
+
+?>
 
 
 <!DOCTYPE html>
@@ -21,10 +54,12 @@ try {
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/main_connexion.css">
+    <script src="https://kit.fontawesome.com/81aa4e8517.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <title></title>
 
 
@@ -34,10 +69,12 @@ try {
             <img src="../image/logo64.png" alt="" class="rounded-circle">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-bars fa-lg" style="color:#e3001b; font-size:28px;"></i>
+            </span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <a class="nav-link " href="accueil.php">Accueil </a>
                 </li>
@@ -84,30 +121,30 @@ try {
 
     <div class="container-fluid">
         <div class="row">
-            <form class="form shadow-inset-center">
+            <form class="form shadow-inset-center" method="post" sname="formrdv">
                 <h1>
-                    <i class="fa fa-user-plus fa-lg"></i> Prendre un nouveau rendez-vous</h1>
+                    <i class="fa fa-user-plus fa-lg"></i> Ajouter un nouveau trajet</h1>
                 <br>
                 <br>
                 <div class="form-group">
                     <label for="motif"> Motif</label>
-                    <input type="text" id="motif" class="form-control">
+                    <input type="text" id="motif" name="motif" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="lieu"> Adresse du rendez-vous </label>
-                    <input type="text" id="lieu" class="form-control">
+                    <input type="text" id="lieu" name="lieu" class="form-control">
                 </div>
                 <div class="row">
                     <div class="col">
                         <div class="form-group">
                             <label for="date"> Date </label>
-                            <input type="date" id="date" class="form-control">
+                            <input type="date" id="date" name="date" class="form-control">
                         </div>
                     </div>
                     <div class="col">
                         <div class="form-group">
                             <label for="heure"> Heure du rendez-vous </label>
-                            <input type="time" id="heure" class="form-control">
+                            <input type="time" id="heure" name="heure" class="form-control">
                         </div>
                     </div>
                 </div>
@@ -118,11 +155,11 @@ try {
                         <h3>Départ souhaité</h3>
                         <div class="form-group">
                             <label for="heure_dep"> Heure de départ </label>
-                            <input type="time" id="heure_dep" class="form-control">
+                            <input type="time" id="heure_dep" name="heure_dep" class="form-control">
                         </div>
                         <div class="form-group">
                             <label for="adresse_dep"> Adresse de départ </label>
-                            <input type="text" id="date" class="form-control">
+                            <input type="text" id="adresse_dep" name="adresse_dep" class="form-control">
                         </div>
 
                     </div>
@@ -130,21 +167,25 @@ try {
                         <h3>Retour prévu</h3>
                         <div class="form-group">
                             <label for="heure_ret"> Heure de rétour </label>
-                            <input type="time" id="heure_ret" class="form-control">
+                            <input type="time" id="heure_ret" name="heure_ret" class="form-control">
                         </div>
                     </div>
                 </div>
+                <?php if (isset($erreur)) {
+                    echo '<font color ="red">' . $erreur . "</font>";
+                } ?>
+
                 <br>
                 <div class="row">
                     <div class="col">
-                        <input type="submit" id='submit' value='Enregistrer'>
+                        <input type="submit" id='submit' value='Enregistrer' name="formrdv">
                     </div>
                     <div class="col">
                         <input type="submit" id='submit' value='Annuler'>
                     </div>
             </form>
-            </div>
         </div>
+    </div>
 
 
 </body>
